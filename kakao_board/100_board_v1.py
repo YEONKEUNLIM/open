a0af8b8102bf76da9aca114c2ecf0cc00e64e038
lotuslim <lotuslim000@gmail.com>
2022-08-31 23:52:06 +0900

    5개씩 올리도록 커밋

--- kakao_board/100_board_v1.py
+++ kakao_board/100_board_v1.py
@@ -93,7 +93,8 @@ def check_board_full(conn, boardNm):
 
 
 #보드 등록
-def insert_board(conn, data, browser):
+def insert_board(conn, data, browser, CurCount, MaxCount):
+    
     print(data)
     print("browser session : ",browser)
     print("추가할 보드 url")
@@ -108,78 +109,88 @@ def insert_board(conn, data, browser):
     print("카테고리 19 : humor")
     category = str(data[5]) #category = "19"
 
-    returnCode = False
+    if CurCount == 0 : 
+        print("################# 보드 발행 START ################")
+        print("보드 대시보드 이동")
+        browser.get(kakaoUrl+boardUrl)
 
-    print("################# 보드 발행 START ################")
-    print("보드 대시보드 이동")
-    browser.get(kakaoUrl+boardUrl)
+        sleep(3)
+        print("보드 등록")
+        browser.find_element(By.CLASS_NAME,"link_newboard").click()
 
-    sleep(3)
-    print("보드 등록")
-    browser.find_element(By.CLASS_NAME,"link_newboard").click()
+        #만약 10개 보드로 꽉찼으면? 팝업 닫고 continue 진행한다. 
 
-    #만약 10개 보드로 꽉찼으면? 팝업 닫고 continue 진행한다. 
-
-    sleep(1)
-    print("보드 제목 입력")
-    boardTitle = browser.find_element(By.ID, "boardTitle")
-    boardTitle.send_keys(title)
-    sleep(1)
-    print("보드 설명 입력")
-    boardCmt = browser.find_element(By.ID, "boardCmt")
-    boardCmt.send_keys(comment)
-    sleep(1)
-    print("보드 링크 탭 선택")
-    browser.find_element(By.XPATH,"//*[@id='mainContent']/div[2]/div/div[2]/div[2]/ul/li[2]/a").click()
-    sleep(1)
+        sleep(1)
+        print("보드 제목 입력")
+        boardTitle = browser.find_element(By.ID, "boardTitle")
+        boardTitle.send_keys(title)
+        sleep(1)
+        print("보드 설명 입력")
+        boardCmt = browser.find_element(By.ID, "boardCmt")
+        boardCmt.send_keys(comment)
+        sleep(1)
+        print("보드 링크 탭 선택")
+        browser.find_element(By.XPATH,"//*[@id='mainContent']/div[2]/div/div[2]/div[2]/ul/li[2]/a").click()
+        sleep(1)
+    
+    if CurCount > 0 :
+        sleep(1)
+        print("링크 삭제")
+        browser.find_element(By.CLASS_NAME,"btn_del").click()
+        
+    
     print("링크 입력")
     url = browser.find_element(By.NAME, "url")
     url.send_keys(contentsLink)
+
     sleep(1)
     print("링크 검색")
     browser.find_element(By.CLASS_NAME,"btn_search").click()
     sleep(3)
     print("보드 담기")
     browser.find_element(By.XPATH,"//*[@id='mainContent']/div[2]/div/div[2]/div[3]/form/div[2]/ul/li/div[2]/button").click()
-    sleep(2)
-    print("발행하기")
-    browser.find_element(By.XPATH,"//*[@id='mainContent']/div[3]/div[2]/button[2]").click()
-    sleep(2)
-    print("발행설정 > 카테고리(선택)")
-    browser.find_element(By.XPATH,"//*[@id='layer']/div/div/div[2]/div/div[3]/dl/dd/div/div["+category+"]/label").click()
-    sleep(1)
-    print("최종 발행하기")
-    browser.find_element(By.XPATH,"//*[@id='layer']/div/div/div[2]/div/div[5]/button[2]").click()
-    sleep(5)
-    print("오픈전 팝업나올때 처리.")
-    #browser.find_element(By.XPATH,"//*[@id='layer']/div/div/div[3]/div/button").click()
-    popupTxt = browser.find_element(By.XPATH,"//*[@id='layer']/div/div/div[1]/strong").text
-    print(popupTxt)
-    if popupTxt == '하루에 너무 많은 보드를 발행하고 있어요.':
-        #DB에 해당 보드 개수 10 만들기
-        print("#############BOARD_MASTER 현재 발생 개수 최대치 개수로 수정 START#############")
-        add_complete_update2(conn, data[0])
-        print("#############BOARD_MASTER 현재 발생 개수 최대치 개수로 수정 END#############")
-        #팝업 Close
-        browser.execute_script("document.getElementsByClassName('btn_g btn_primary')[1].click()")
+    
+    CurCount = CurCount+1
+    if CurCount == MaxCount : 
+        sleep(2)
+        print("발행하기")
+        browser.find_element(By.XPATH,"//*[@id='mainContent']/div[3]/div[2]/button[2]").click()
+        sleep(2)
+        print("발행설정 > 카테고리(선택)")
+        browser.find_element(By.XPATH,"//*[@id='layer']/div/div/div[2]/div/div[3]/dl/dd/div/div["+category+"]/label").click()
         sleep(1)
-        #현재창 초기화
-        browser.find_element(By.XPATH,"//*[@id='root']/div[2]/main/section/aside/nav/ul/li[2]/a").click()
-        sleep(1)
-        browser.find_element(By.XPATH,"//*[@id='layer']/div/div/div[3]/div/button[2]").click()
-        
-    else :
-        
-        print("#############LINK_INFO UPLOAD_FLAG Y 처리 & BOARD_MASTER Count + 1 START#############")
-        add_complete_update1(conn, data[0], data[6])
-        print("#############LINK_INFO UPLOAD_FLAG Y 처리 & BOARD_MASTER Count + 1 END#############")
+        print("최종 발행하기")
+        browser.find_element(By.XPATH,"//*[@id='layer']/div/div/div[2]/div/div[5]/button[2]").click()
+        sleep(5)
+        print("오픈전 팝업나올때 처리.")
+        #browser.find_element(By.XPATH,"//*[@id='layer']/div/div/div[3]/div/button").click()
+        popupTxt = browser.find_element(By.XPATH,"//*[@id='layer']/div/div/div[1]/strong").text
+        print(popupTxt)
+        if popupTxt == '하루에 너무 많은 보드를 발행하고 있어요.':
+            #DB에 해당 보드 개수 10 만들기
+            print("#############BOARD_MASTER 현재 발생 개수 최대치 개수로 수정 START#############")
+            add_complete_update2(conn, data[0])
+            print("#############BOARD_MASTER 현재 발생 개수 최대치 개수로 수정 END#############")
+            #팝업 Close
+            browser.execute_script("document.getElementsByClassName('btn_g btn_primary')[1].click()")
+            sleep(1)
+            #현재창 초기화
+            browser.find_element(By.XPATH,"//*[@id='root']/div[2]/main/section/aside/nav/ul/li[2]/a").click()
+            sleep(1)
+            browser.find_element(By.XPATH,"//*[@id='layer']/div/div/div[3]/div/button[2]").click()
+            
+        else :
+            
+            print("#############LINK_INFO UPLOAD_FLAG Y 처리 & BOARD_MASTER Count + 1 START#############")
+            add_complete_update1(conn, data[0], data[6])
+            print("#############LINK_INFO UPLOAD_FLAG Y 처리 & BOARD_MASTER Count + 1 END#############")
 
-        browser.execute_script("document.getElementsByClassName('btn_g btn_primary')[1].click()")
+            browser.execute_script("document.getElementsByClassName('btn_g btn_primary')[1].click()")
 
 
     print("################# 보드 발행 END ##################")
-    returnCode = True
-    return returnCode
+
+    return CurCount
 
 #Main() Board 발행 횟수 초기화 
 def init_board_curcnt_zero(conn) :
@@ -224,16 +235,22 @@ def board_run(conn,browser) :
     if data == [] :
         print("등록된 data가 없습니다. skip", data)
     
+    curCount = 0
+    maxCount = 5
     #카카오 보드에 입력
     for tmp in data :
         print("보드 발행 max 인지 체크 시작", tmp)
         cnt = check_board_full(conn, tmp[0]).fetchone()[0]
         print("0 : 보드 발행 가능, 1:보드 Full 발행 불가능 : ",cnt)
         print("보드 발행 max 인지 체크 종료")
+        
         if cnt > 0 :
             continue
         else :
-            insert_board(conn,tmp,browser)
+            curCount = insert_board(conn,tmp,browser, curCount, maxCount)
+        
+        if curCount == maxCount : 
+            curCount = 0
             #try :
             #    insert_board(tmp,browser)
             #except Exception as e :
